0570defff1b00a0ae5de8bfb3f1358ad786cf440
raymondgonzalez <collegeeduraymond@gmail.com>
2025-01-16 15:01:08 -0800

    I reworded and added the feature making the program interative using string code

--- COMP003A.LectureActivity1Rider/Program.cs
+++ COMP003A.LectureActivity1Rider/Program.cs
@@ -6,10 +6,24 @@
         //mail level
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Enter your name: Raymond\nHello Rodrigo? My name is Raymond Gonzalez\n\n\nHere are 10 things about me:\n 1. My name starts with an R.\n 2.I am a son.\n 3.I have a math class.\n 4.I am an SI.\n 5. I go to Lemoore Collge\n 6.I have a sister named Maria\n 7. I have another sister names Susie\n 8. Lastly I have one more sister names Lupe\n 9.I forgot to mention I have an older brother\n 10. My brothers's name is Jose");
+            Console.Write("Enter your name: "); // Prompt the user
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "! My name is Raymond \n \n" ); 
+
+            Console.WriteLine("Here are 10 things about me:"); 
+            Console.WriteLine("        1. My name starts with an R."); 
+            Console.WriteLine("        1. I am a son."); 
+            Console.WriteLine("        1. I have a math class."); 
+            Console.WriteLine("        1. I am an SI."); 
+            Console.WriteLine("        1. I go to Lemoore College."); 
+            Console.WriteLine("        1. I have a sister named Maria."); 
+            Console.WriteLine("        1. I have another sister named Susie."); 
+            Console.WriteLine("        1. Lastly, I one more sister names Lupe."); 
+            Console.WriteLine("        1. I forgot to mention, I have an older brother."); 
+            Console.WriteLine("        1. My brother's name is Jose."); 
             /*string username = Console.ReadLine(); // gets the user input and save it to a variable names username
-            Console.WriteLine("Hi" + username + ".My name is Raymond");*/ //Using this as notes on a differnt way of doing things
+            Console.WriteLine("Hi" + username + ".My name is Raymond");*/
+            //Using this as notes on a different way of doing things
 
         }
     }
